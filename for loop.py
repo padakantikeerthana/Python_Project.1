6dc604c43362cb002e44b9b465501a98182916cd
Dell <padakantikeerthana@gmail.com>
2026-05-20 22:51:24 +0530

    new file

---
+++ for loop.py
@@ -0,0 +1,10 @@
+for  item in [1,2,3,4,5,6,7,8,9.0]:
+    print(item)
+
+prices=[10,20,30]
+total=0
+for price in prices:
+    total+=price
+print(f"total:{total}")
+
+
---
+++ lists.py
@@ -0,0 +1,14 @@
+names =['keerthana','manveer','raju','mahitha']
+print(names)
+
+names =['keerthana','manveer','raju','mahitha']
+names[0] ='keerthan'
+print(names[2: ])
+print(names)
+
+numbers=[3,4,2,25,6]
+max = numbers[0]
+for number in numbers:
+    if number>max:
+        max = number
+print(max)
